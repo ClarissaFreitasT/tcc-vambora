1eead8652580ab5ed61ecae3d1d1bc452701aa75
hfcosta <hfcosta>
2026-09-03 16:38:42 -0300

    instalando bcrypt e jwt, ajustando banco de dados

---
+++ backend/prisma/migrations/20260903193553_removendo_orcamento_perfil/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `orcamento_perfil` on the `usuarios` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `usuarios` DROP COLUMN `orcamento_perfil`;
